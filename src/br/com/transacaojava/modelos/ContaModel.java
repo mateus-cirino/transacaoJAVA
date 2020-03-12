43cbaa29108b6ac4bb34a978275aa08eb1cd500e
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-03-11 21:29:20 -0300

    criacao da conta Model

---
+++ src/br/com/transacaojava/modelos/ContaModel.java
@@ -0,0 +1,52 @@
+package br.com.transacaojava.modelos;
+
+public class ContaModel {
+    private int id;
+    private String descricao;
+    private Double saldo;
+
+    /**
+     * @return the descricao
+     */
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public Double getSaldo() {
+        return saldo;
+    }
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String toString() {
+        return "codigo: " + this.id + " descricao: " + this.descricao + " saldo: " + this.saldo;
+    }
+}
