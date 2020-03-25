17ba1ddb67ff643b68523730d891b06c73234922
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-03-25 00:58:47 -0300

    model extrato com tipo Enum

---
+++ src/br/com/transacaojava/modelos/Extrato.java
@@ -0,0 +1,63 @@
+package br.com.transacaojava.modelos;
+
+public class Extrato {
+    private Integer id;
+    private String descricao;
+    private Double valor;
+    public enum Conta {
+        D("Débito"),
+        C("Credito");
+     
+        private String descricao;
+     
+        Conta(String descricao) {
+            this.descricao = descricao;
+        }
+     
+        public String getDescricao() {
+            return descricao;
+        }
+    }
+    private Conta tipo;
+    private int idConta;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public Double getValor() {
+        return this.valor;
+    }
+
+    public Conta getTipo() {
+        return this.tipo;
+    }
+
+    public int getidConta() {
+        return this.idConta;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public void setTipo(Conta tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setidConta(int idConta) {
+        this.idConta = idConta;
+    }
+}
--- src/br/com/transacaojava/modelos/ExtratoModel.java
+++
@@ -1,9 +0,0 @@
-package br.com.transacaojava.modelos;
-
-public class ExtratoModel {
-    private Integer id;
-    private String descricao;
-    private Double valor;
-    private Enum tipo;
-    private Conta conta;
-}
