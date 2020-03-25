612041b67d0462f5de78e932306f35723c686f1e
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-03-25 00:58:22 -0300

    Testando o dao do extrato

---
+++ src/br/com/transacaojava/testes/TesteExtrato.java
@@ -0,0 +1,26 @@
+package br.com.transacaojava.testes;
+
+import java.util.Collection;
+import br.com.transacaojava.dao.*;
+import br.com.transacaojava.modelos.*;
+
+public class TesteExtrato {
+    public static void main(String[] args) {
+
+        ExtratoDao extratoDao = new ExtratoDao();
+
+        Collection<Extrato> extratos = extratoDao.select(1);
+
+        extratos.forEach((e) -> System.out.println(e.getTipo()));
+
+        Extrato extrato = new Extrato();
+
+        extrato.setId(2);
+        extrato.setDescricao("Extrato2");
+        extrato.setValor(10.0);
+        extrato.setTipo(Extrato.Conta.D);
+        extrato.setidConta(17);
+
+        extratoDao.update(extrato);
+    }
+}
