19ea975c88fdf769fd37d630d541465de1f8ecee
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-03-31 11:46:27 -0300

    criacao de uma fachada para metodos mais complexos na conta

---
+++ src/br/com/transacaojava/dao/fachadas/ContaFachada.java
@@ -0,0 +1,49 @@
+package br.com.transacaojava.dao.fachadas;
+
+import java.sql.Connection;
+
+import br.com.transacaojava.conexao.FabricaConexaoTransacional;
+import br.com.transacaojava.dao.ContaDao;
+import br.com.transacaojava.dao.ExtratoDao;
+import br.com.transacaojava.modelos.Conta;
+import br.com.transacaojava.modelos.Extrato;
+
+public class ContaFachada {
+    public static void saca (Conta contaASerSacada, Double valorASerSacado, Connection conexao) {
+        ContaDao contaDao = new ContaDao(conexao);
+         
+        if(contaDao.select(contaASerSacada.getId()).isPresent()){
+            try {
+                Extrato extrato = new Extrato();
+                extrato.setDescricao("Saque da conta "
+                                    + contaASerSacada.getId());
+                extrato.setTipo(Extrato.Operacao.S);  
+                extrato.setValor(valorASerSacado);
+                extrato.setidConta(contaASerSacada.getId());
+    
+                ExtratoDao extratoDao = new ExtratoDao(conexao);
+                extratoDao.inserir(extrato);
+    
+                Conta conta = contaDao.select(contaASerSacada.getId()).get();
+                conta.setSaldo(conta.getSaldo() - valorASerSacado);
+                contaDao.update(conta);
+    
+                FabricaConexaoTransacional.commitTransacao(conexao);
+                FabricaConexaoTransacional.closeConnection(conexao);
+            } catch (Exception e) {
+                FabricaConexaoTransacional.rollbackTransacao(conexao);
+                FabricaConexaoTransacional.closeConnection(conexao);
+                throw new RuntimeException("Não foi possível efetuar a operacao de "
+                                            + "saque"
+                                            + "\nError: " + e.getMessage());
+            }
+            
+        }else{
+            throw new RuntimeException("Não existe uma conta associada a este"
+                                + " id no banco");
+        }
+
+
+        
+    }
+}
