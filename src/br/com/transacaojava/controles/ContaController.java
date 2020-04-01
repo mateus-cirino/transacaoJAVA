d8f27808d2abc8e4ceca5ebf365d1c008b792793
Mateus Cirino <mateuscirinosilva32@gmail.com>
2020-04-01 15:48:12 -0300

    refatoracao do controller

--- src/br/com/transacaojava/controles/ContaController.java
+++ src/br/com/transacaojava/controles/ContaController.java
@@ -1,48 +1,27 @@
 package br.com.transacaojava.controles;
 
-
-import java.util.Optional;
-
 import br.com.transacaojava.dao.ContaDao;
-import br.com.transacaojava.modelos.Conta;
 
 public class ContaController {
 
-    public void testeTransacao(int idContaASerDepositada, Double rendimento) {
-        ContaDao contaDao = new ContaDao();
-        
-        //Optional<Conta> conta = contaDao.select(idContaASerSacada);
-        Optional<Conta> conta2 = contaDao.select(idContaASerDepositada);
-
-        if(conta2.isPresent()) {
-            Conta contaASerDepositada = conta2.get();
-            contaDao.rendimento(contaASerDepositada, rendimento);
-        }
-
-        /*if(conta.isPresent() && conta2.isPresent()) {
-            Conta contaASerSacada = conta.get();
-            Conta contaASerDepositada = conta2.get();
-
-            contaDao.transferencia(contaASerSacada, contaASerDepositada, valorASerSacado);
-        }*/
-
-        
-        /*if(conta.isPresent()) {
-            Conta contaASerDepositada = conta.get();
-            Double valorASerDepositado = 1054.0;
-            
-            contaDao.deposita(contaASerDepositada, valorASerDepositado);
-        }*/
-
-        /*if(conta.isPresent()) {
-            Conta contaASerSacada = conta.get();
-            Double valorASerSacado = 100.0;
-            
-            contaDao.saca(contaASerSacada, valorASerSacado);
-        }else {
-            throw new RuntimeException("Não foi encontrada a conta de id: "
-                            + idContaASerSacada);
-        }*/
-
+    public void saca(int idContaASerSacada, double valorASerSacado) {
+        ContaDao contaDao = new ContaDao(); 
+        contaDao.saca(idContaASerSacada, valorASerSacado);
     }
+
+    public void deposita(int idContaASerDepositada, double valorASerDepositado) {
+        ContaDao contaDao = new ContaDao(); 
+        contaDao.deposita(idContaASerDepositada, valorASerDepositado);
+    }
+
+    public void transferencia(int idContaASerSacada, int idContaASerDepositada, double valorASerSacado) {
+        ContaDao contaDao = new ContaDao();
+        contaDao.transferencia(idContaASerSacada, idContaASerDepositada, valorASerSacado);
+    }
+
+    public void rendimento(int idContaASerDepositada, double rendimento) {
+        ContaDao contaDao = new ContaDao();
+        contaDao.rendimento(idContaASerDepositada, rendimento);
+    }
+
 }
